a834213e7785442d487480ac0a80ce48c7d580a4
princewillopah <princewillopah@gmail.com>
2024-09-20 07:53:52 +0100

    new push

---
+++ Basics/script2.groovy
@@ -0,0 +1,13 @@
+def buildJar() {
+    echo "building the application...... 89% ..."
+} 
+
+def buildImage() {
+    echo "building the docker image... 89% ..."
+} 
+
+def deployApp() {
+    echo 'deploying the application...... 89% ... '
+} 
+
+return this
